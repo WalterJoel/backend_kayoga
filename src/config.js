70b692974b9606d7ad7851cf5924ee91c70683cd
WalterJoel <walter.valdivia@ucsp.edu.pe>
2022-11-22 20:05:01 -0500

    Terminando la implementacion de la produccion de inyeccion

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ console.log(process.env.PORT);
 
 // con proces.env.port le digo que use la variable de entorno del archivo.env o sino ||
 export const PORT      = process.env.PORT ||  '4000';
-export const DB_PORT      = process.env.DB_PORT ||    '7621';
+export const DB_PORT      = process.env.DB_PORT || '7621';
 export const DB_USER      = process.env.DB_USER || 'root';
 export const DB_PASSWORD  = process.env.DB_PASSWORD || 'G56ql30tZnwcIVRW8OjI';
 export const DB_HOST      = process.env.DB_HOST || 'containers-us-west-21.railway.app';
