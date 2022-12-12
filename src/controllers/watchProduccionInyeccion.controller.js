50387aaeda0c153de838bfb41d1ec3935a384dcb
WalterJoel <walter.valdivia@ucsp.edu.pe>
2022-12-12 16:13:11 -0500

    S

--- src/controllers/watchProduccionInyeccion.controller.js
+++ src/controllers/watchProduccionInyeccion.controller.js
@@ -18,25 +18,26 @@ export const getOrdenInyeccion = async(req,res)=>{
 1.-  Guarda en formato JSON toda la informacion de la orden generada 
 */
 export const saveOrdenInyeccion = async (req, res) => {
+       
         try {
-            //orden_inyeccion_json es todo el json devuelto por eso no uso { }
-            const orden_inyeccion_json =req.body;
-             // itero por las cantidades para obtener el total de pares
-             let pares_orden = 0;
-             orden_inyeccion_json.map((pares)=>{
-                pares_orden += pares['cantidad'];
-             })
+                //orden_inyeccion_json es todo el json devuelto por eso no uso { }
+                const orden_inyeccion_json =req.body;
+                // itero por las cantidades para obtener el total de pares
+                let pares_orden = 0;
+                orden_inyeccion_json.map((pares)=>{
+                        pares_orden += pares['cantidad'];
+                })
 
-            let fecha_creacion = new Date();
-            fecha_creacion=fecha_creacion.toLocaleString('PET',{timeZone:'America/Lima'});
-            const [produccion_inyeccion]= await pool.query(` 
-                        INSERT INTO watch_produccion_inyeccion
-                        (orden_inyeccion,fecha_creacion,pares_orden)
-                        VALUES (?, ?, ?)`,
-                        [JSON.stringify(orden_inyeccion_json),fecha_creacion,pares_orden]);
-            res.json(produccion_inyeccion)            
+                let fecha_creacion = new Date();
+                fecha_creacion=fecha_creacion.toLocaleString('PET',{timeZone:'America/Lima'});
+                const [produccion_inyeccion]= await pool.query(` 
+                                INSERT INTO watch_produccion_inyeccion
+                                (orden_inyeccion,fecha_creacion,pares_orden)
+                                VALUES (?, ?, ?)`,
+                                [JSON.stringify(orden_inyeccion_json),fecha_creacion,pares_orden]);
+                res.json(produccion_inyeccion)            
         } catch (error) {
-              return res.status(500).json({
+                return res.status(500).json({
                 message:'Algo anda mal al insertar en la tabla watch y seriado restante', error})    
         }
 };
@@ -48,73 +49,103 @@ export const saveOrdenInyeccion = async (req, res) => {
 1.-  Los maquinistas cierran la orden                
 2.- Actualizamos la tabla watch_produccion_inyeccion agregando fecha_cierre_orden y pares_inyectados
 */
+async function getAndUpdateCantidades(idwatch){
+        const [total_insertos]= await pool.query(` 
+                SELECT 
+                SUM(talla1+talla2+talla3+talla4+talla5) AS mySUM
+                FROM insertos`);
+        const [total_zapatillas]= await pool.query(`
+                SELECT 
+                SUM(talla1+talla2+talla21+talla3+talla31+talla4+talla41+talla5+talla51) AS mySUM
+                FROM zapatillas
+                `);
+        const [total_cortes]= await pool.query(`SELECT 
+                SUM(talla1+talla2+talla21+talla3+talla31+talla4+talla41+talla5+talla51) AS mySUM
+                FROM seriado_restante
+                `);
+        if(total_insertos&&total_zapatillas&&total_cortes){
+                const [rows]= await pool.query(`UPDATE watch_produccion_inyeccion
+                                                SET 
+                                                total_insertos_inicio   = ${total_insertos[0].mySUM},
+                                                total_zapatillas_inicio = ${total_zapatillas[0].mySUM},
+                                                total_cortes_inicio     = ${total_cortes[0].mySUM}
+                                                WHERE watch_produccion_inyeccion.idwatch_produccion_inyeccion=${idwatch}` ); 
+                return rows;
+        }                
+}
 export const saveOrdenInyeccionMaquinista= async (req, res) => {
-        
-        // Esta variable validara si se han insertado todos los datos al comparar con el len del array                
         try {
-            const orden_inyeccion_json =req.body;
-            
-            //Total pares que cuenta el maquinista
-             let pares_inyectados = 0;
-             let idwatch_produccion_inyeccion = 0;
-             orden_inyeccion_json.map((pares)=>{
-                pares_inyectados += parseInt(pares['cantidad']);
-                idwatch_produccion_inyeccion = pares.idwatch_produccion_inyeccion;
-             })
-            let fecha_cierre_orden = new Date();
-            fecha_cierre_orden=fecha_cierre_orden.toLocaleString('PET',{timeZone:'America/Lima'});
-            //Actualizo la tabla watch_produccion_inyeccion 1 sola vez
-            const [rows]= await pool.query(`UPDATE watch_produccion_inyeccion
-                SET estado_orden='CERRADA', fecha_cierre_orden='${fecha_cierre_orden}',
-                pares_inyectados = ${pares_inyectados}
-                WHERE watch_produccion_inyeccion.idwatch_produccion_inyeccion=${idwatch_produccion_inyeccion}` );
-            // Actualizo los cortes y los insertos uno por uno
-            orden_inyeccion_json.map(async(pares,i,arr)=>{
-                let idseriadorestante = pares.idseriadorestante;
-                let idinserto         = pares.idinserto;
-                let name_talla        = pares.talla_insert;
-        //Acorto la talla21 a talla2, porque los insertos son tallas fijas 26 28. etc
-                let name_talla_for_inserto = name_talla.substr(0,6);
-                let cantidad          = pares.cantidad;
-                let idmodelo          = pares.idmodelo;
-                // Primero intento actualizar los cortes
-                try {
-                        const [cortes]= await pool.query(`UPDATE seriado_restante
-                        SET ${name_talla}= (ifnull(${name_talla},0)-${cantidad}) 
-                        WHERE seriado_restante.idseriadorestante=${idseriadorestante}`);
-                        // Segundo intento actualizar los insertos
-                        try {
-                                const [insertos]= await pool.query(`UPDATE insertos
-                                SET ${name_talla_for_inserto}= (ifnull(${name_talla_for_inserto},0)-${cantidad}) 
-                                WHERE insertos.idinserto=${idinserto}`);
-                                // Tercero, intento actualizar el stock de zapatillas
+                const orden_inyeccion_json =req.body;
+                
+                //Total pares que cuenta el maquinista
+                let pares_inyectados = 0;
+                let idwatch_produccion_inyeccion = 0;
+                //let
+                orden_inyeccion_json.map(async(pares)=>{
+                        pares_inyectados += parseInt(pares['cantidad']);
+                        idwatch_produccion_inyeccion = pares.idwatch_produccion_inyeccion;
+                })
+                //Si agrego los totales
+                const [agregar_datos_watch]= await getAndUpdateCantidades(idwatch_produccion_inyeccion);
+                if(agregar_datos_watch.length){
+                        let fecha_cierre_orden = new Date();
+                        fecha_cierre_orden=fecha_cierre_orden.toLocaleString('PET',{timeZone:'America/Lima'});
+                        //Actualizo la tabla watch_produccion_inyeccion 1 sola vez
+                        const [rows]= await pool.query(`UPDATE watch_produccion_inyeccion
+                                SET estado_orden='CERRADA', 
+                                fecha_cierre_orden='${fecha_cierre_orden}',
+                                pares_inyectados = ${pares_inyectados}
+                                WHERE watch_produccion_inyeccion.idwatch_produccion_inyeccion=${idwatch_produccion_inyeccion}` );
+                        // Actualizo los cortes y los insertos uno por uno
+                        orden_inyeccion_json.map(async(pares,i,arr)=>{
+                                let idseriadorestante = pares.idseriadorestante;
+                                let idinserto         = pares.idinserto;
+                                let name_talla        = pares.talla_insert;
+                        //Acorto la talla21 a talla2, porque los insertos son tallas fijas 26 28. etc
+                                let name_talla_for_inserto = name_talla.substr(0,6);
+                                let cantidad          = parseInt(pares.cantidad);
+                                let idmodelo          = pares.idmodelo;
+                                // Primero intento actualizar los cortes
                                 try {
-                                        const [zapatillas]= await pool.query(`UPDATE zapatillas
-                                        SET ${name_talla}= (ifnull( ${name_talla}, 0 )+ ${cantidad}) 
-                                        WHERE zapatillas.idmodelo=${idmodelo}`);
-                                        if(arr.length-1 === i){
-                                                res.json(zapatillas);
+                                        const [cortes]= await pool.query(`UPDATE seriado_restante
+                                        SET ${name_talla}= (ifnull(${name_talla},0)-${cantidad}) 
+                                        WHERE seriado_restante.idseriadorestante=${idseriadorestante}`);
+                                        // Segundo intento actualizar los insertos
+                                        try {
+                                                const [insertos]= await pool.query(`UPDATE insertos
+                                                SET ${name_talla_for_inserto}= (ifnull(${name_talla_for_inserto},0)-${cantidad}) 
+                                                WHERE insertos.idinserto=${idinserto}`);
+                                                // Tercero, intento actualizar el stock de zapatillas
+                                                try {
+                                                        const [zapatillas]= await pool.query(`UPDATE zapatillas
+                                                        SET ${name_talla}= (ifnull( ${name_talla}, 0 )+ ${cantidad}) 
+                                                        WHERE zapatillas.idmodelo=${idmodelo}`);
+                                                        if(arr.length-1 === i){
+                                                                res.json(zapatillas);
+                                                        }
+                                                } catch (error) {
+                                                        console.log('eroor ',error)
+                                                        return res.status(500).json({
+                                                        message:'Error al actualizar el stock de zapatillas', error})                    
+                                                }
+                                        } catch (error) {
+                                                console.log('eroor ',error)
+                                                return res.status(500).json({
+                                                message:'Error al actualizar los insertos', error})                    
                                         }
+
                                 } catch (error) {
                                         console.log('eroor ',error)
                                         return res.status(500).json({
-                                        message:'Error al actualizar el stock de zapatillas', error})                    
+                                        message:'Error al actualizar los cortes', error})    
                                 }
-                        } catch (error) {
-                                console.log('eroor ',error)
-                                return res.status(500).json({
-                                message:'Error al actualizar los insertos', error})                    
-                        }
-
-                } catch (error) {
-                        console.log('eroor ',error)
-                        return res.status(500).json({
-                        message:'Error al actualizar los cortes', error})    
+                        })
                 }
-             })
-        } catch (error) {
+
+        } 
+        catch (error) {
                 console.log(error, ' Error al modificar el watch inyeccion')
-              return res.status(500).json({
+        return res.status(500).json({
                 message:'Algo anda mal al insertar en la tabla watch y seriado restante', error})    
         }
 };
